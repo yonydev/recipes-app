62bcd98b934618e884ce93913b99f76d70200215
Yonatan Rojas <yonatan@bedu.org>
2018-02-20 12:56:40 -0600

    added info to recipe-list; styled recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Esto es una prueba', 'Esta es la descripción de la prueba', 'http://chinesefood-eye.com/wp-content/uploads/2016/03/PF-Changs-Kung-Pao-Chicken.jpg')
+  ];
   
   constructor() { }
 
