511b3f872bf38f13186fff867bc94a802df8407b
Yonatan Rojas <yonatan@bedu.org>
2018-03-27 12:02:21 -0600

    Passed data with Event and Property Binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Esto es una prueba', 'Esta es la descripción de la prueba', 'http://chinesefood-eye.com/wp-content/uploads/2016/03/PF-Changs-Kung-Pao-Chicken.jpg')
+    new Recipe('Esto es una prueba', 'Esta es la descripción de la prueba', 'http://chinesefood-eye.com/wp-content/uploads/2016/03/PF-Changs-Kung-Pao-Chicken.jpg'),
+    new Recipe('Esto es otra prueba', 'Esta es la descripción de la otra prueba', 'http://chinesefood-eye.com/wp-content/uploads/2016/03/PF-Changs-Kung-Pao-Chicken.jpg')
   ];
   
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
